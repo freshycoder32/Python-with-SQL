be7bf21c1de09f42b1900e4117df9b1d7f28218f
FreshyCoder <pimganyapat@gmail.com>
2020-07-29 15:31:52 +0700

    Update WeekOfMonth.sql
    add week of year col

--- WeekOfMonth.sql
+++ WeekOfMonth.sql
@@ -1,3 +1,5 @@
 DECLARE @thisdate date = '2020-07-29'
 
-select (DATEPART(week, @thisdate) - DATEPART(week, DATEADD(day, 1, EOMONTH(@thisdate, -1)))) + 1 as WeekOfMonth
+select
+   (DATEPART(week, @thisdate) as WeekOfYear
+   ,(DATEPART(week, @thisdate) - DATEPART(week, DATEADD(day, 1, EOMONTH(@thisdate, -1)))) + 1 as WeekOfMonth
