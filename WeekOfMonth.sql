2a9deedaf670e8cacdba7914e1bdb6e4c8a5a996
FreshyCoder <pimganyapat@gmail.com>
2020-07-29 15:43:17 +0700

    Update WeekOfMonth.sql
    add IS_WEEKEND , IS_WEEKDAY

--- WeekOfMonth.sql
+++ WeekOfMonth.sql
@@ -3,3 +3,5 @@ DECLARE @thisdate date = '2020-07-29'
 select
    (DATEPART(week, @thisdate) as WeekOfYear
    ,(DATEPART(week, @thisdate) - DATEPART(week, DATEADD(day, 1, EOMONTH(@thisdate, -1)))) + 1 as WeekOfMonth
+   ,case when DATEPART(weekday, @date_given) between 2 and 6 then 1 else 0 end as IS_WEEKDAY
+   ,case when DATEPART(weekday, @date_given) in (1,7) then 1 else 0 end as IS_WEEKEND
